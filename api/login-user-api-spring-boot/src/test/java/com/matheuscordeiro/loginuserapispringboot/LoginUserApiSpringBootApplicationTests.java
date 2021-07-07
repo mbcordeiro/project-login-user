ea8f7728d8a1f11e2f1bb51ebbac67e29e7d7f5c
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-07-07 15:23:13 -0300

    First commit

---
+++ api/login-user-api-spring-boot/src/test/java/com/matheuscordeiro/loginuserapispringboot/LoginUserApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.loginuserapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginUserApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
