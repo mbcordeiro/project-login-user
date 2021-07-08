3d9b9ca2b8528ced7c68c64084c6363cdb830057
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-07-08 18:25:36 -0300

    User entity

---
+++ api/login-user-api-spring-boot/src/main/java/com/matheuscordeiro/loginuserapispringboot/entities/User.java
@@ -0,0 +1,26 @@
+package com.matheuscordeiro.loginuserapispringboot.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String password;
+}
