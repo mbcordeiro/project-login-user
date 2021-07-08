95c2d4e9ebe31724e6884e12fe2f98f05aabb38f
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-07-08 18:27:18 -0300

    User repository

---
+++ api/login-user-api-spring-boot/src/main/java/com/matheuscordeiro/loginuserapispringboot/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.matheuscordeiro.loginuserapispringboot.repositories;
+
+import com.matheuscordeiro.loginuserapispringboot.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
